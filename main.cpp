d8f60636352d1873e20e83cf6790f8c3f196f36d
RogueGenXTechy <dlmunoz45@gmail.com>
2025-03-29 16:56:48 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/*
+    . Project description
+        . Topic #1
+        . Topic #2
+*/
+// This will comment out the entire line
+
+import utilities;
+
+int main() {
+
+  app();
+
+  return 0;
+}
